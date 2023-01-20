4a18e47cd9143cc30d721a1a57ed770aa795c263
JuanJose <jhonajose@hotmail.es>
2023-01-20 17:57:43 +0100

    Initial Commit

---
+++ FindMyBurger/FindMyBurgerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindMyBurgerApp.swift
+//  FindMyBurger
+//
+//  Created by Juan jose Morales on 20/1/23.
+//
+
+import SwiftUI
+
+@main
+struct FindMyBurgerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
